d1cc3da9ff9301f82fdabf5ed0b039e0bd093933
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-03-13 14:48:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tawheedraheem::Application.config.secret_token = '99a54b0d3ef5b719c7e9afcc3b8b5c370c2ecebac35e58e16482552e7d20210d1703c37d7a86a287552b8f7bfb7a52a67f3cc5c9ff173bcc9b8965b2c3b180db'
