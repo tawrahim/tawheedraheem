d7c08eadede04eae45162a0d041084a03aa6c1ac
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-03-21 20:31:25 -0400

    Yeah buddy! it works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,11 @@ Tawheedraheem::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.assets.debug = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
